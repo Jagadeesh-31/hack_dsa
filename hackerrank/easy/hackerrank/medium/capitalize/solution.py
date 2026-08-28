c8636a113e5d3551c9d10552394d5dcb80f87656
Jagadeesh-31 <22b91a6133srkrecacin@gmail.com>
2026-08-28 21:04:57 +0530

    Delete hackerrank/easy/hackerrank directory

--- hackerrank/easy/hackerrank/medium/capitalize/solution.py
+++
@@ -1,5 +0,0 @@
-
-
-# Complete the solve function below.
-def solve(s):
-     return re.sub(r'(^|\s)([a-z])', lambda match: match.group(1) + match.group(2).upper(), s)
--- hackerrank/easy/hackerrank/medium/python-string-split-and-join/solution.py
+++
@@ -1,3 +0,0 @@
-def split_and_join(line):
-    # write your code here
-     return "-".join(line.split(" "))
